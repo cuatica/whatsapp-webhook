68c7a5b3fbdb1b8c5f12bdc29d9153eeea69bd83
Natalia Rojas <nataroj@amazon.com>
2025-10-19 19:50:57 -0700

    update verify token

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from pywa import WhatsApp
 app = Flask(__name__)
 
 wa = WhatsApp(
-    phone_id="YOUR_PHONE_NUMBER_ID",
-    token="YOUR_ACCESS_TOKEN",
-    verify_token="my-secret",
+    phone_id="883020854886533",
+    token="EAAlHsOZCxr6gBPgQnoscOJgvoqZA1AXxZA8dlCbjDkfeGxRNtSlta5n0NXEIUkM2ZAYdy6ZAOD1aRKKpXnKCBt4h74D1EWFdubHaOGcaV2E303xC7banQL0BfeEPZBtMPZCs6Tk951h62gZAknBMPpJor6YD1WYl0T2npLd6gfFkVwUr9OTTf7cHOt4tlGZBCbx3ZB3uqQIEC3ZARYhwrh93pZC2m5s465u8Uqyrae1yTpwn2jkZD",
+    verify_token="Cobi14",
     server=app
 )
 
